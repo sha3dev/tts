0cc687226f28917653cf4d5ec8f96b71f7fa5b94
jc <root@sha3.dev>
2024-07-18 00:20:27 +0200

    fix(config): env vars names

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,16 @@
 export default {
   // elevenlabs
   ELEVENLABS_TTS_API_URL:
-    process.env.TTS_API_URL || "https://api.elevenlabs.io/v1/text-to-speech",
+    process.env.ELEVENLABS_TTS_API_URL ||
+    "https://api.elevenlabs.io/v1/text-to-speech",
   ELEVENLABS_SOUND_API_URL:
-    process.env.SOUND_API_URL ||
+    process.env.ELEVENLABS_SOUND_API_URL ||
     "https://api.elevenlabs.io/v1/sound-generation",
-  ELEVENLABS_MAX_SOUND_DURATION: Number(process.env.MAX_SOUND_DURATION || 4),
+  ELEVENLABS_MAX_SOUND_DURATION: Number(
+    process.env.ELEVENLABS_MAX_SOUND_DURATION || 4
+  ),
   ELEVENLABS_DEFAULT_MODEL_ID:
-    process.env.DEFAULT_MODEL_ID || "eleven_multilingual_v2",
+    process.env.ELEVENLABS_DEFAULT_MODEL_ID || "eleven_multilingual_v2",
   // polly
   POLLY_DEFAULT_ENGINE: process.env.POLLY_DEFAULT_ENGINE || "neural",
 };
